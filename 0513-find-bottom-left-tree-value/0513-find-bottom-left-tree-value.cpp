08274c0a1a3b6b3d78378a6b0f756f41306211d1
Kumkum Chauhan <kumkumchauhan32625@gmail.com>
2026-09-21 15:45:43 +0530

    Time: 0 ms (100%), Space: 25.1 MB (46.56%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -14,7 +14,7 @@ public:
     int findBottomLeftValue(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        int ans = root->val;
+        int bottom_left_value = root->val;
         
         while(!q.empty()){
             int size = q.size();
@@ -23,12 +23,12 @@ public:
                 q.pop();
                 
                 
-                if(i == 0) ans = node->val;
+                if(i == 0) bottom_left_value = node->val;
                 
                 if(node->left) q.push(node->left);
                 if(node->right) q.push(node->right);
             }
         }
-        return ans;
+        return bottom_left_value;
     }
 };
